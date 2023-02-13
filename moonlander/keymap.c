5e53f7bde931eaa7ec8adea2ce4f121c7515e51d
andym <git@11001110001101101000011001000110.org>
2023-02-13 21:37:33 +0000

    real hyper

--- moonlander/keymap.c
+++ moonlander/keymap.c
@@ -60,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_EQUAL,       LT(1,KC_1),     LT(2,KC_2),     LT(3,KC_3),     KC_4,           KC_5,           KC_LCBR,                                        KC_RCBR,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_MAC_COPY,                                    KC_MAC_PASTE,   KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,      
-    KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_F18,                                                                         ST_MACRO_0,     KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
+    KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           ALL_T(KC_ESCAPE),                                                                ST_MACRO_0,     KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,      
-    KC_F18,         KC_GRAVE,       KC_LALT,        KC_LBRACKET,    KC_RBRACKET,    MO(1),                                                                                                          MO(2),          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       LGUI(KC_SPACE), 
+    ALL_T(KC_ESCAPE),KC_GRAVE,       KC_LALT,        KC_LBRACKET,    KC_RBRACKET,    MO(1),                                                                                                          MO(2),          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       LGUI(KC_SPACE), 
     KC_SPACE,       KC_BSPACE,      KC_PGUP,                        KC_PGDOWN,      KC_LGUI,        KC_ENTER
   ),
   [1] = LAYOUT_moonlander(
